9857d3454dafcca934ecf2444c3e4947564dc9ba
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 01:22:10 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+/*
 const corsOptions = {
   origin: [
     'https://mesto.iapina.nomoredomains.club',
@@ -39,7 +40,42 @@ const corsOptions = {
   ],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
+*/
+
+const allowedCors = [
+  'https://mesto.iapina.nomoredomains.club',
+  'http://mesto.iapina.nomoredomains.club',
+  'http://178.154.246.154',
+  'http://localhost:3000',
+]
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+}
+
+  next();
+});
+
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
@@ -71,7 +107,7 @@ app.post('/signin', celebrate({
 }), login);
 
 // авторизация
-//app.use(auth);
+app.use(auth);
 // все что ниже - закрыто авторизацией
 
 app.use('/users', userRouter);
