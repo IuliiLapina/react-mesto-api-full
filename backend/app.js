1c24930aebd99c8f9a1691225ebf5bbc3094ec4b
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 12:55:51 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-/*
+
 const corsOptions = {
   origin: [
     'https://mesto.iapina.nomoredomains.club',
@@ -42,7 +42,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-*/
+
 //app.options('*', cors());
 /*
 const allowedCors = [
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
   next();
 });
 */
-app.use(cors());
+//app.use(cors());
 
 app.use(express.json());
 app.use(helmet());
