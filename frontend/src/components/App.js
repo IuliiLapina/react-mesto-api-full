2394310f17d1124bee80084ca35b626f43c06501
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 17:43:40 +0300

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -154,7 +154,7 @@ React.useEffect(() => {
   //добавление / удаление лайков на карточках
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = [card.likes].some((i) => i === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     const likeToChange = isLiked
       ? api.deleteCardLike(card._id)
