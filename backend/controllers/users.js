2897b4246e0cbd1757640866ae29676bbce6c070
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 22:27:20 +0300

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { NODE_ENV, JWT_SECRET } = process.env;
-
+const { NODE_ENV } = process.env;
+const { JWT_SECRET } = require('../config.env');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // require('dotenv').config();
