190121d25b0fcde31dd9c7495207c2ee0c13d39b
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 14:47:19 +0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,24 +1,3 @@
-const checkResponse = (res) => {
-    if (res.ok) {
-      return res.json(); 
-    }
-    return Promise.reject(`Ошибка ${res.status}`);
-  }
-
-  //если токен действителен, вернёт ответ с информацией о пользователе
-export const getContent = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    }
-  })
-  .then(checkResponse)
-  .then(data => data)
-} 
-
-
 export const BASE_URL = 'http://backend.mesto.iapina.nomoredomains.club';
 
 export const register = (email, password) => {
@@ -37,17 +16,37 @@ export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({email, password})
   })
   .then(checkResponse)
-  .then((data) => {
+ /* .then((data) => {
     if (data.token){
-      localStorage.setItem('jwt', token);
+      localStorage.setItem('jwt', data.token);
       return data.token;
     } 
   })
   .catch(err => console.log(err))
+  */
+};
+
+  //если токен действителен, вернёт ответ с информацией о пользователе
+export const getContent = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+  .then(checkResponse)
+  //.then(data => data)
+};
+
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json(); 
+  }
+  return Promise.reject(`Ошибка ${res.status}`);
 };
