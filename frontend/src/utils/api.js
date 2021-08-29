28a022257ce6d9f452ad07770caa71c2776fce07
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-08-29 12:26:46 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 class Api {
   constructor({ address, token }) {
     this._address = address;
-    this._token = token;
+    this._headers = headers;
   }
 
   _checkResponse(res) {
@@ -14,80 +14,78 @@ class Api {
   //получить инфо пользователя с сервера
   getUserData() {
     return fetch(`${this._address}/users/me`, {
-      headers: {
-        authorization: this._token,
-      },
-    }).then(this._checkResponse);
+      headers: this._headers,
+      credentials: 'include',
+    })
+    .then(this._checkResponse);
   }
 
   //получить карточки с сервера
   getInitialCards() {
     return fetch(`${this._address}/cards`, {
-      headers: {
-        authorization: this._token,
-      },
-    }).then(this._checkResponse);
+      headers: this._headers,
+      credentials: 'include',
+      })
+      .then(this._checkResponse);
   }
 
   setUserData(data) {
     return fetch(`${this._address}/users/me`, {
-      method: "PATCH",
-      headers: {
-        authorization: this._token,
-        "Content-type": "application/json",
-      },
+      method: 'PATCH',
+      headers: this._headers,
+      credentials: 'include',
       body: JSON.stringify({
         name: data.name,
-        about: data.about,
-      }),
-    }).then(this._checkResponse);
+        about: data.about
+      })
+    })
+    .then(this._checkResponse);
   }
 
   setUserAvatar(link) {
     return fetch(`${this._address}/users/me/avatar`, {
-      method: "PATCH",
-      headers: {
-        authorization: this._token,
-        "Content-type": "application/json",
-      },
+      method: 'PATCH',
+      headers: this._headers,
+      credentials: 'include',
       body: JSON.stringify({
-        avatar: link,
-      }),
-    }).then(this._checkResponse);
+        avatar: link.avatar
+      })
+    })
+    .then(this._checkResponse);
   }
 
   addNewCard(data) {
     return fetch(`${this._address}/cards`, {
-      method: "POST",
-      headers: {
-        authorization: this._token,
-        "Content-type": "application/json",
-      },
+      method: 'POST',
+      headers: this._headers,
+      credentials: 'include',
       body: JSON.stringify({
         name: data.name,
-        link: data.link,
-      }),
-    }).then(this._checkResponse);
+        link: data.link
+      })
+    })
+    .then(this._checkResponse)
   }
 
   addCardLike(cardId) {
     return fetch(`${this._address}/cards/likes/${cardId}`, {
-      method: "PUT",
-      headers: {
-        authorization: this._token,
-      },
-    }).then(this._checkResponse);
+      method: 'PUT',
+      headers: this._headers,
+      credentials: 'include',
+      })
+      .then(this._checkResponse);
   }
 
   deleteCardLike(id) {
-    return fetch(`${this._address}/cards/likes/${id}`, {
-      method: "DELETE",
-      headers: {
-        authorization: this._token,
-      },
-    }).then(this._checkResponse);
+    return fetch(`${this._address}/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this._headers,
+      credentials: 'include',
+      })
+      .then(this._checkResponse);
   }
 
+  /*
   deleteCard(cardId) {
     return fetch(`${this._address}/cards/${cardId}`, {
       method: "DELETE",
@@ -96,10 +94,23 @@ class Api {
       },
     }).then(this._checkResponse);
   }
+  */
+
+  deleteCard(cardId) {
+    return fetch(`${this._address}/cards/${cardId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+    })
+    .then(this._checkResponse);
+  }
 }
 
 const api = new Api({
   address: "http://backend.mesto.iapina.nomoredomains.club",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+  },
 //  token: "7a45c432-7073-4f3b-9cf1-c12940fb64b9",
 });
 
