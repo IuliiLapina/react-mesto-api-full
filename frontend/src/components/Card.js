39b062ed0ca431205f5b98255f90f2d310113a70
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-09-01 22:46:39 +0300

    Update Card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete}) {
   const currentUser = React.useContext(CurrentUserContext);
 
   // Определяем, являемся ли мы владельцем текущей карточки
-  const isOwn = (card.owner === currentUser._id);
+  const isOwn = (card.owner === currentUser.data._id);
   console.log(`isOwn = ${isOwn}`)
   console.log(`card.owner = ${card.owner}`)
   console.log(`currentUser._id = ${currentUser._id}`)
