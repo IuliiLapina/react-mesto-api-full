f5b515bdfc3099845fd5fc866cc5316545847ec9
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-09-02 12:44:30 +0300

    Update Card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,7 +6,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete}) {
 
   // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = (card.owner === (currentUser.data && currentUser.data._id));
- // console.log(`currentUser = ${currentUser}`)
+// console.log(`currentUser = ${currentUser}`)
 //  console.log(`currentUser.data = ${currentUser.data}`)
 
   // Создаём переменную, которую после зададим в `className` для кнопки удаления
@@ -14,7 +14,8 @@ function Card({ card, onCardClick, onCardLike, onCardDelete}) {
   `${isOwn ? 'card__delete-btn' : 'card__delete-btn_hidden'}`); 
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some(like => (like === (currentUser.data && currentUser.data._id)));
+  const isLiked = card.likes.some(like => like === (currentUser.data && currentUser.data._id));
+  console.log(`card.likes= ${card.likes}`)
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = (`card__like-btn ${isLiked ? 'card__like-btn_active' : ''}`); 
