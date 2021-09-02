6e75bc5d1fce7f2b54585bfa9bfa51ffc42242e8
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-09-02 18:47:01 +0300

    Update cards.js

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -20,7 +20,7 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().hex().length(24),
+    cardId: Joi.string().hex().length(24),
   }),
 }), deleteCard);
 
